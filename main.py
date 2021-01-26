7ed970075cb5dbe85f400996f46c58622cbd335d
Koraboev Oyatullo <korabeov.o@gmail.com>
2021-01-26 21:40:48 +0500

    Third function added

--- main.py
+++ main.py
@@ -21,25 +21,31 @@ def res1(arg):
 dif_currency = res1('latest?base=RUB')
 
 
-def res1(arg):
+def res2(arg):
     var = requests.get(f'https://api.exchangeratesapi.io/{arg}')
     data = json.loads(var.text)
     return data
 
 
+character = res2('latest?symbols=USD,GBP')
 
 
+def res3(arg):
+    var = requests.get(f'https://api.exchangeratesapi.io/{arg}')
+    data = json.loads(var.text)
+    return data
 
 
-
-
+dates = res3('history?start_at=2018-01-01&end_at=2018-01-07')
 
 
 
 
 dictionary = {
     "Difference": difference,
-    'Different currency': dif_currency
+    'Different currency': dif_currency,
+    'Different_character': character,
+    'Different_dates': dates,
 }
 
 json_object = json.dumps(dictionary, indent=4)
