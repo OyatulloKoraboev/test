d3740718e6dc9d9896cdc44f80644c283c07a29b
Koraboev Oyatullo <korabeov.o@gmail.com>
2021-01-26 21:28:43 +0500

    Second function added

--- main.py
+++ main.py
@@ -4,15 +4,42 @@ from pprint import pprint
 
 
 def res(arg):
-    response = requests.get(f'https://api.exchangeratesapi.io/{arg}')
-    data = json.loads(response.text)
+    var = requests.get(f'https://api.exchangeratesapi.io/{arg}')
+    data = json.loads(var.text)
     return data["rates"]['USD']
 
 
 difference = res('2021-01-26') - res('1999-01-26')
 
+
+def res1(arg):
+    var = requests.get(f'https://api.exchangeratesapi.io/{arg}')
+    data = json.loads(var.text)
+    return data
+
+
+dif_currency = res1('latest?base=RUB')
+
+
+def res1(arg):
+    var = requests.get(f'https://api.exchangeratesapi.io/{arg}')
+    data = json.loads(var.text)
+    return data
+
+
+
+
+
+
+
+
+
+
+
+
 dictionary = {
-    "difference": difference,
+    "Difference": difference,
+    'Different currency': dif_currency
 }
 
 json_object = json.dumps(dictionary, indent=4)
